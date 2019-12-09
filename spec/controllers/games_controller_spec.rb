958ae5989bbfd79c902a69642af8f3b0f2b23d17
clubgorlando <eric.gorland@gmail.com>
2019-12-09 05:52:18 +0000

    user sign in now redirects to games#index page

---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe GamesController, type: :controller do
+  # describe "games#index action" do
+  #   it "should require users to be logged in before showing page"
+  #     get :index
+  #     expect(response).to redirect_to new_user_session_path
+  #   end
+
+  # end
+end
